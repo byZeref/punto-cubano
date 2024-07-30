3479a2acb300d4efd53c4a59a55af0ed3b650358
Sandor SC <sandorsc941018@gmail.com>
2024-07-30 13:02:01 -0400

    login design

--- components/navbar/CompanyDesktop.vue
+++ components/navbar/CompanyDesktop.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <NuxtLink to="/" class="logo-name flex items-center gap-2 max-lg:hidden">
+  <NuxtLink to="/" class="logo-name flex items-center gap-1 max-lg:hidden">
     <img class="logo-img w-[60px] h-auto" src="/logo.png" alt="logo-app">
     <span class="font-bold text-xl">Punto Cubano</span>
   </NuxtLink>
--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -7,6 +7,8 @@ definePageMeta({
 const { AUTH_API_ERROR } = error_types
 const authStore = useAuthStore()
 const router = useRouter()
+const passwordType = ref('password')
+const loading = ref(false)
 const form = ref({
   email: 'sandorsc941018@gmail.com',
   password: 'Zeref.cubano',
@@ -14,7 +16,9 @@ const form = ref({
 
 const login = async () => {
   const payload = { ...form.value }
+  loading.value = true
   const { data, error } = await authStore.login(payload)
+    .finally(() => { loading.value = false })
   if (error) {
     console.error('error on login', error)
     // TODO handle error with modal and extract logic to new file
@@ -27,34 +31,69 @@ const login = async () => {
   }
 }
 
+const togglePasswordType = () => {
+  passwordType.value = passwordType.value === 'password' ? 'text' : 'password'
+}
+
+const btnUI = {
+  color: {
+    primary: {
+      solid: `shadow-sm text-white dark:text-white bg-[#4c1b1e] hover:bg-[#4c1b1ef0] disabled:bg-[#4c1b1e] 
+      dark:bg-[#4c1b1e] dark:hover:bg-[#4c1b1ed0] dark:disabled:bg-[#4c1b1e]`,
+    },
+  }
+}
 </script>
 
 <template>
-  <!-- TODO fix design -->
   <div class="flex flex-col justify-center items-center h-screen">
-    <UCard>
-      <h3 class="text-2xl text-center">Login Page</h3>
-      <form @submit.prevent="login">
-        <div class="max-w-xs flex flex-col gap-4 items-center mx-auto mt-4">
+    <UCard class="w-[500px] max-w-[90%]">
+      <div class="logo-container flex items-center justify-center gap-1">
+        <img class="logo-img w-[160px] h-auto" src="/logo.png" alt="logo-app">
+      </div>
+      <div class="w-full px-2">
+        <h3 class="text-center text-slate-800 dark:text-white text-lg leading-[24px] ">
+          Ingrese sus credenciales para iniciar sesión
+        </h3>
+      </div>
+      <form @submit.prevent="login" class="pb-6">
+        <div class="max-w-[90%] flex flex-col gap-4 items-center mx-auto mt-4">
           <UInput
             v-model="form.email"
-            color="cyan"
             type="text"
-            size="sm"
+            color="white"
+            size="xl"
             icon="i-heroicons-envelope" 
             placeholder="Usuario"
             class="w-full"
-            />
+            :disabled="loading"
+          />
             <UInput
             v-model="form.password"
-            color="cyan"
-            type="password"
-            size="sm"
+            :type="passwordType"
+            color="white"
+            size="xl"
             icon="i-heroicons-lock-closed" 
             placeholder="Contraseña"
             class="w-full"
-          />
-          <UButton type="submit" color="cyan" block>
+            :disabled="loading"
+          >
+            <template #trailing>
+              <UIcon
+                v-if="passwordType === 'password'"
+                @click="togglePasswordType"
+                name="i-heroicons-eye"
+                class="w-5 h-5 text-slate-400 active:scale-[0.85] duration-200" 
+              />
+              <UIcon
+                v-else
+                @click="togglePasswordType"
+                name="i-heroicons-eye-slash"
+                class="w-5 h-5 text-slate-400 active:scale-[0.85] duration-200" 
+              />
+            </template>
+          </UInput>
+          <UButton type="submit" color="primary" size="xl" block :ui="btnUI" :disabled="loading">
             <span class="text-center">Iniciar Sesión</span>
           </UButton>
         </div>
@@ -64,7 +103,8 @@ const login = async () => {
 </template>
 
 <style scoped>
-.navbar {
-  padding: 0 calc(20vw);
+:deep(span.absolute.inset-y-0.end-0.pointer-events-none) {
+  pointer-events: all !important;
+  cursor: pointer !important;
 }
 </style>
