37e8a628fdcfd9b7742b7ce176601e19917c58cc
Sandor SC <sandorsc941018@gmail.com>
2024-08-15 15:43:20 -0400

    white logo in dark mode

--- components/navbar/CompanyDesktop.vue
+++ components/navbar/CompanyDesktop.vue
@@ -7,9 +7,8 @@ defineProps({
 
 <template>
   <NuxtLink to="/" class="logo-name flex items-center gap-1 max-lg:hidden">
-    <!-- <img v-if="isDarkMode" class="logo-img w-[60px] h-auto" src="/logo-dark.png" alt="logo-app">
-    <img v-else class="logo-img w-[60px] h-auto" src="/logo.png" alt="logo-app"> -->
-    <img class="logo-img w-[60px] h-auto" src="/logo.png" alt="logo-app">
+    <img v-show="isDarkMode" class="logo-img w-[60px] h-auto" src="/logo-white.png" alt="logo-app">
+    <img v-show="!isDarkMode" class="logo-img w-[60px] h-auto" src="/logo.png" alt="logo-app">
     <span class="font-bold text-xl">Punto Cubano</span>
   </NuxtLink>
 </template>
--- components/navbar/CompanyMobile.vue
+++ components/navbar/CompanyMobile.vue
@@ -17,7 +17,8 @@ const props = defineProps({
   <div class="lg:hidden flex items-center gap-2">
     <IconMenu @click="$emit('open')" v-if="showTriggerButton" :color="isDarkMode ? '#fff' : '#7f2c30'" /> 
     <NuxtLink to="/" class="logo-name flex items-center gap-1">
-      <img class="logo-img w-[50px] h-auto" src="/logo.png" alt="logo-app">
+      <img v-show="isDarkMode" class="logo-img w-[50px] h-auto" src="/logo-white.png" alt="logo-app">
+      <img v-show="!isDarkMode" class="logo-img w-[50px] h-auto" src="/logo.png" alt="logo-app">
       <span class="font-bold text-[18px]">Punto Cubano</span>
     </NuxtLink>
   </div>
