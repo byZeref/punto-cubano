d0f541317969432becf382520c83eee1ae7bc316
Sandor SC <sandorsc941018@gmail.com>
2024-08-23 16:11:57 -0400

    button ui

--- utils/constants.ts
+++ utils/constants.ts
@@ -21,3 +21,14 @@ export const regex = {
   PRICE_REGEX: /^[1-9]\d*(\.\d+)?$/,
   EMAIL_REGEX: /^[\w.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
 }
+
+export const buttons_ui = {
+  BTN_PRIMARY: {
+    color: {
+      primary: {
+        solid: `shadow-sm text-white dark:text-white bg-[#4c1b1e] hover:bg-[#4c1b1ef0] disabled:bg-[#4c1b1e] 
+        dark:bg-[#4c1b1e] dark:hover:bg-[#4c1b1ed0] dark:disabled:bg-[#4c1b1e]`,
+      },
+    }
+  },
+}
